baab29b57fa6fb9fc87331a762d7ce18681e592f
YASWANTH BATTU <83590022+s-1-n-t-h@users.noreply.github.com>
2023-07-02 14:54:39 +0530

    simplechain

---
+++ JavaScript/SimpleChain.ts
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import { OpenAI } from "langchain/llms/openai";
+
+export const run = async () => {
+    const model = new OpenAI({ temperature: 0.1 });
+    const res = await model.call("What is the capital city of France?");
+    console.log(res);
+};
+
+run();
